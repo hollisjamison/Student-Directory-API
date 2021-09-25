ec0e1e63b5c305d6fee58f7f0cf276858688c8ee
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-25 10:32:12 -0400

    create port for listening on 1800 and test route for get request

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const PORT = 1800
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send(' TEST The get request is working')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`) // eslint-disable-line no-console
+})
