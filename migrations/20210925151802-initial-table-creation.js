bb4bcf84c47803f977bfa85c6efd266385053f82
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-25 11:38:44 -0400

    add migration code for the initial table creation

--- migrations/20210925151802-initial-table-creation.js
+++ migrations/20210925151802-initial-table-creation.js
@@ -2,20 +2,26 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+    await queryInterface.createTable('students', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING },
+      age: { type: Sequelize.INTEGER },
+      address: { type: Sequelize.STRING },
+      gpa: { type: Sequelize.FLOAT },
+      major: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+    await queryInterface.dropTable('students')
   }
 };
