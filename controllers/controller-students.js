c140d291f345e51ee7a584857d7712d39144b538
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-25 10:43:22 -0400

    move function for get request into the controller-students file

--- controllers/controller-students.js
+++ controllers/controller-students.js
@@ -2,6 +2,11 @@ const errorFunction = (req, res) => {
   res.status(404).send('ERROR CODE 404 PAGE NOT FOUND');
 };
 
+const getStudents = (req, res) => {
+  res.send('TEST This will be a list of all students');
+};
+
 module.exports = {
-  errorFunction
+  errorFunction,
+  getStudents
 }
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { errorFunction } = require('./controllers/controller-students')
+const { errorFunction, getStudents } = require('./controllers/controller-students')
 
 const PORT = 1800
 const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send(' TEST The get request is working')
-})
+app.get('/', getStudents)
 
 app.get('*', errorFunction)
 
