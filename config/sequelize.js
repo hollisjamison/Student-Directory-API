6c45c4c5870850f02d4796d346c1e578c2d6d5e7
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-10-14 09:02:36 -0400

    add to config file

--- config/sequelize.js
+++ config/sequelize.js
@@ -6,4 +6,11 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'mysql'
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
 }
